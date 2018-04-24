29e89745f44fab7be2c624911917e7f122e174c9
Chaitanya Khanna <c2khanna@uwaterloo.ca>
2018-04-24 16:13:31 -0400

    added about me

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgsRevealModule } from 'ng-scrollreveal';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    HomeComponent
+    HomeComponent,
+    AboutMeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgsRevealModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
